bb54a1daf65309b4d2096b3018ad4a57e7a4068a
Neha Kazi <47070642+nk398@users.noreply.github.com>
2022-03-26 12:34:30 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// API KEY = b72c25db9b704d3a8bd6795549500944
+
+import './App.css';
+
+import React, { Component } from 'react'
+import News from './components/News';
+import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
+
+export default class App extends Component {
+  apiKey = "b72c25db9b704d3a8bd6795549500944";
+  pageSize = 6;
+  country = 'in';
+
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({progress : progress})
+  }
+  render() {
+    return (
+      <div> 
+        <LoadingBar
+        color='#f11946'
+          progress={this.state.progress}
+          height={3}
+        />
+        
+        <Routes>
+          <Route exact path="/" element={ <News setProgress={this.setProgress} key={1} pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category='general'/> } />
+         
+        </Routes>
+      </div>
+      
+    )
+  }
+}
+
