c3bb09b2e8bfea6792a418251ee434f9eea5091c
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-03-29 11:47:33 -0400

    settings fail test case

--- src/settings.test.js
+++ src/settings.test.js
@@ -7,21 +7,12 @@ afterEach(cleanup)
 render(<settings />);
 screen.getByText('settings');
 
-test('onsubmit signup', () => {
-  const handleSubmit = jest.fn()
+test('refresh button', () => {
+  const { button } = jest.fn()
  // const { } = render(<login onSubmit={handleSubmit} />)
-  fireEvent.change(screen.getByLabelText(/username/i), {target: {value: 'johndoe'}})
-  fireEvent.change(screen.getByLabelText(/email/i), {target: {value: 'johndoe@gmail.com'}})
-  fireEvent.change(screen.getByLabelText(/password/i), {target: {value: 'password@123'}})
 
-  screen.getByText(/signup/i).click()
+  screen.getByText(/refresh/i).click()
 
-  expect(handleSubmit).toHaveBeenCalledTimes(1)
-  expect(handleSubmit).toHaveBeenCalledWith({
-    username: 'johndoe',
-    email: 'johndoe@gmail.com',
-    password: 'password'
   })
-})
 
   
