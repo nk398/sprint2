5bb1b0fa50a3c01719894c9d4c87a20aae9065c9
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-03-29 15:49:56 -0400

    update login test

--- src/login.test.js
+++ src/login.test.js
@@ -3,9 +3,35 @@ import React from 'react'
 //import { screen } from '@testing-library/react';
 //import { render, fireEvent, cleanup } from '@testing-library/react'
 import { render } from '@testing-library/react';
+//import { screen, fireEvent} from '@testing-library/react'
+//import { getByText } from '@testing-library/jest-dom';
+//import { JSDOM } from 'jsdom'
+
+//const email = screen.getByPlaceholderText('Email Address')
+
 
 describe('App component loads correctly', () => {
   test('renders correctly', () => {
       render(<login />);
+  })
+})
+
+
+describe("Test login form", function() {
+ 
+  it("email check", function() {
+
+  })
+
+  it("password check", function() {
+
+  })
+
+  it("login", function() {
+
+  })
+
+  it("signup", function() {
+
   })
 })
