3ad96a64588c873196b239b5976f3f259ae2f137
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-03-29 15:49:11 -0400

    update settings test

--- src/settings.test.js
+++ src/settings.test.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup,screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 afterEach(cleanup)
 
 describe('App component loads correctly', () => {
     test('renders correctly', () => {
         render(<settings />);
-
     })
-
 })
+
+test('click', () => {
+    render(
+      <div>
+        <label htmlFor="checkbox">Check</label>
+        <input id="checkbox" type="checkbox" />
+      </div>,
+    )
+  
+    userEvent.click(screen.getByText('Check'))
+    expect(screen.getByLabelText('Check')).toBeChecked()
+  })
