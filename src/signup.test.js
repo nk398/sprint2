4b1de33689bc60913ae54ba5cff60dae3855b04c
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-03-29 12:53:33 -0400

    signup.test.js

---
+++ src/signup.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import { screen } from '@testing-library/any-framework';
+//import { screen } from '@testing-library/react';
+//import { render, fireEvent, cleanup } from '@testing-library/react'
+import { render } from '@testing-library/react';
+
+describe('App component loads correctly', () => {
+  test('renders correctly', () => {
+      render(<signup />);
+  })
+})
