6dbc0fb3cea617b7044e9c5cb7993e2d143b2ef5
Neha Kazi <47070642+nk398@users.noreply.github.com>
2022-04-09 12:03:01 -0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDxrbU1u49R7Rmd4BxUkO-RSBTY4shdfM4",
+  authDomain: "auth-development-605b4.firebaseapp.com",
+  databaseURL: "https://auth-development-605b4-default-rtdb.firebaseio.com",
+  projectId: "auth-development-605b4",
+  storageBucket: "auth-development-605b4.appspot.com",
+  messagingSenderId: "1016616956444",
+  appId: "1:1016616956444:web:93064645917026b76365d9"
+})
+
+export const auth = app.auth()
+export default app
