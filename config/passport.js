b90747d363cdf25a3565793c04bfe196cc896f64
Neha Kazi <47070642+nk398@users.noreply.github.com>
2022-04-17 02:34:11 -0400

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../controller/models/user');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
