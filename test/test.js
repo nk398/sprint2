7d668c1223605c8a590ba782949028804ccff79c
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-04-18 15:03:33 -0400

    update pass first test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,12 @@ const User = require('../controller/models/User')
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const expect = chai.expect;
+const should = chai.should();
 
 chai.use(chaiHttp); 
 
- describe('/First Test', () =>{
+ describe('First Test', () =>{
     it('test welcome', (done) =>{
         chai.request(app)
         .get('/')
