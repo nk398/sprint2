055b170a756b8606126b5d18126d3e2dc8657434
NikitaGaikwad01 <81545664+NikitaGaikwad01@users.noreply.github.com>
2022-04-18 14:57:22 -0400

    testing initial first page

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const User = require('../controller/models/User')
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp); 
+
+ describe('/First Test', () =>{
+    it('test welcome', (done) =>{
+        chai.request(app)
+        .get('/')
+        .end((err,res) => {
+         res.should.have.status(200);
+            done();
+        });
+});
+ })
